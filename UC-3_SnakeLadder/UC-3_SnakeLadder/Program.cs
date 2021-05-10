f0c8ce21b2e624804fddc7b92085b90181b64381
Sharath <Sharus9587@gmail.com>
2021-05-10 19:01:09 +0530

    Added : Player checks for option

---
+++ UC-3_SnakeLadder/UC-3_SnakeLadder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UC_3_SnakeLadder
+{
+    class Program
+    {
+            public class SnakeAndLadder
+            {
+                public const int IsLadder = 1;
+                public const int IsSnake = 2;
+
+
+                static void Main(string[] args)
+                {
+                    int posOfPlayer = 0;
+                    Random random = new Random();
+                    int dice = random.Next(1, 7);
+                    int option = random.Next(0, 3);
+                    switch (option)
+                    {
+                        case IsLadder:
+                            posOfPlayer += dice;
+                            break;
+
+                        case IsSnake:
+                            posOfPlayer -= dice;
+                            if (posOfPlayer < 0)
+                                posOfPlayer = 0;
+                            break;
+
+                        default: break;
+                    }
+                    Console.WriteLine("The player is at position " + posOfPlayer + ",and rolled a die : " + dice);
+                }
+
+            }
+        }
+    }
